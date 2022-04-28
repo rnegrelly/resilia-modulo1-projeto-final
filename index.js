e23fbcec71412a748b19c8de6cd1254fd1a6710f
Renato Negrelly <renatonegrelly@gmail.com>
2022-04-28 10:15:31 -0300

    atualização-links-e-js

---
+++ index.js
@@ -0,0 +1,14 @@
+function startHercules(){
+
+    window.location = '/modulo1/clone-projeto-modulo1/resilia-modulo1-projeto-final/Hercules/html/hercules-intro.html';
+}
+
+function startHermes(){
+
+    window.location = '/modulo1/clone-projeto-modulo1/resilia-modulo1-projeto-final/Hermes/html/hermes-intro.html';
+}
+
+function startMedusa(){
+
+    window.location = '/modulo1/clone-projeto-modulo1/resilia-modulo1-projeto-final/Medusa/html/medusa-intro.html';
+}
